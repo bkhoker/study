05164007517378c19e167ce00e9e4ac7f7a1ff78
bkhoker <bkhoker@yahoo.com>
2024-02-06 23:46:31 -0500

    mroe

--- Javascript/scripts/11-todo-list.js
+++ Javascript/scripts/11-todo-list.js
@@ -1,4 +1,12 @@
-const todolist = [];
+const todolist = [1 , 2];
+
+let todoLListHTML = [];
+for (let i = 0; i < todolist.length; i++){
+  const todo = todolist[i];
+  const html = `<p>${todo}</p>`;
+  todoLListHTML += html;
+}
+console.log(todoLListHTML);
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input')
